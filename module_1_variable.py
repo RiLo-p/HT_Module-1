f06f01ff2e087ff899c4c55b5440a697c3b4a20a
RiLo-p <wiwer.alemoure@gmail.com>
2024-12-20 22:05:18 +0300

    Add files via upload

---
+++ module_1_variable.py
@@ -0,0 +1,5 @@
+Number_of_tasks = 12
+Number_of_hours = 1.5
+Course_name = 'Python'
+Time_for_task = Number_of_hours / Number_of_tasks
+print ('Курс:',Course_name,', всего задач:',Number_of_tasks,', затрачено часов:', Number_of_hours,', среднее время выполнения',Time_for_task,'часа.')
